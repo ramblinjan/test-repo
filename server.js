4c80e1b588b35fd5a721dee16e431a5f61628f20
Jan Jorgensen <jandjorgensen@gmail.com>
2018-06-25 11:07:04 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+
+var db = require("./models");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+// Handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.info(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
+
+module.exports = app;
